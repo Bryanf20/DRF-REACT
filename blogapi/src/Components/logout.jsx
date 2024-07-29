a4afeb95e4fcfda71f4c98d093d97d0ea8e2f31a
Bryanf20 <fongangbryan@gmail.com>
2024-07-29 12:16:19 +0100

    Token based authentication [api]

--- blogapi/src/Components/logout.jsx
+++ blogapi/src/Components/logout.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from "react";
+import axiosInstance from "../axios"
+import { useNavigate } from "react-router-dom"
+
 export default function Logout() {
-    return
+    const navigate = useNavigate();
+
+    useEffect(() => {
+		const response = axiosInstance.post('user/logout/blacklist/', {
+			refresh_token: localStorage.getItem('refresh_token'),
+		});
+		localStorage.removeItem('access_token');
+		localStorage.removeItem('refresh_token');
+		axiosInstance.defaults.headers['Authorization'] = null;
+        // console.log(response); //
+		navigate('/login');
+	});
+
+    return <div>Logout</div>
 }
--- blogapi/src/main.jsx
+++ blogapi/src/main.jsx
@@ -4,14 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import Header from "./Components/header.jsx";
-// import footer from "./Components/footer.jsx";
+import SignUp from "./Components/register.jsx";
+import SignIn from "./Components/login.jsx";
+import Logout from "./Components/logout.jsx";
+// import Footer from "./Components/footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <React.StrictMode>
       <Header />
       <Routes>
-        <Route path="/" Component={App} />
+        <Route exact path="/" Component={App} />
+        <Route path="/register" Component={SignUp} />
+        <Route path="/login" Component={SignIn} />
+        <Route path="/logout" Component={Logout} />
       </Routes>
       {/* <Footer /> */}
     </React.StrictMode>
